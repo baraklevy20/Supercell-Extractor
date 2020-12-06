05e47370dae8dde943d1d1dd27bc77bffa130505
unknown <baraklevy20@gmail.com>
2020-12-06 08:20:38 +0200

    Finish MovieClip structure

--- index.js
+++ index.js
@@ -16,27 +16,41 @@ const checkValidity = (buffer, decompressedBuffer) => {
   return fileHash === computedHash;
 };
 
-const readNormalScFile = (buffer, spritesheets) => {
-  // These are the counts for each unique type in the last loop
-  const type0Count = buffer.readUInt16LE();
-  const type1Count = buffer.readUInt16LE();
-  const type2Count = buffer.readUInt16LE();
-  const type3Count = buffer.readUInt16LE();
-  const type4Count = buffer.readUInt16LE();
+const readString = (buffer) => {
+  const stringLength = buffer.readUInt8();
 
-  // ??
-  buffer.readBuffer(7);
-
-  const numberOfStrings = buffer.readUInt16LE();
-
-  for (let i = 0; i < numberOfStrings; i++) {
-    const value = buffer.readUInt16LE();
+  if (stringLength === 0xff) {
+    return null;
   }
 
-  for (let i = 0; i < numberOfStrings; i++) {
-    const stringLength = buffer.readUInt8();
-    const string = buffer.readString(stringLength, 'utf8');
-    console.log(string);
+  return buffer.readString(stringLength, 'utf8');
+};
+
+const readNormalScFile = (buffer, spritesheets) => {
+  // These are used to verify if you're attempting to read too many shapes/animations
+  const shapesCount = buffer.readUInt16LE();
+  const movieClipsCount = buffer.readUInt16LE();
+  const texturesCount = buffer.readUInt16LE();
+  const textFieldsCount = buffer.readUInt16LE();
+  const transformMatricesCount = buffer.readUInt16LE();
+  const colorTransformsCount = buffer.readUInt16LE();
+
+  // Not used
+  buffer.readBuffer(5);
+
+  const numberOfExports = buffer.readUInt16LE();
+  const exportsIds = [];
+  const exports = {};
+
+  for (let i = 0; i < numberOfExports; i++) {
+    exportsIds.push(buffer.readUInt16LE());
+  }
+
+  for (let i = 0; i < numberOfExports; i++) {
+    const exportLength = buffer.readUInt8();
+    const exportName = buffer.readString(exportLength, 'utf8');
+    console.log(`${exportsIds[i].toString(16)} - ${exportName}`);
+    exports[exportsIds[i]] = exportName;
   }
 
   // some block?
@@ -58,48 +72,107 @@ const readNormalScFile = (buffer, spritesheets) => {
     }
 
     switch (blockType) {
-      case 0x08:
+      case 0x08: {
         const matrix = [
-          [buffer.readInt32LE(), buffer.readInt32LE(), buffer.readInt32LE()],
-          [buffer.readInt32LE(), buffer.readInt32LE(), buffer.readInt32LE()],
+          [buffer.readInt32LE() * 0.001, buffer.readInt32LE() * 0.001, buffer.readInt32LE() * 0.001],
+          [buffer.readInt32LE() * 0.001, buffer.readInt32LE() * 0.05, buffer.readInt32LE() * 0.05],
         ];
+        console.log(matrix);
         break;
+      }
+      // case 0x09: {
+      // //   const colorTransform = {
+      // //     redMultiplier: buffer.readUInt8(),
+      // //     greenMultiplier: buffer.readUInt8(),
+      // //     blueMultiplier: buffer.readUInt8(),
+      // //     redAddition: buffer.readUInt8(),
+      // //     greenAddition: buffer.readUInt8(),
+      // //     blueAddition: buffer.readUInt8(),
+      // //   };
+      // //   console.log(colorTransform);
+      //   break;
+      // }
+      case 0x0c: {
+        const exportId = buffer.readUInt16LE();
+        console.log(exports[exportId]);
+        const frameRate = buffer.readUInt8();
+        const countFrames = buffer.readUInt16LE();
+        const countTriples = buffer.readUInt32LE();
+
+        for (let i = 0; i < countTriples; i++) {
+          const triple = [buffer.readInt16LE(), buffer.readInt16LE(), buffer.readInt16LE()];
+          console.log(triple);
+        }
+
+        const x = buffer.readUInt16LE();
+        for (let i = 0; i < x; i++) {
+          const num = buffer.readInt16LE();
+        }
+        for (let i = 0; i < x; i++) {
+          const num = buffer.readUInt8();
+        }
+
+        for (let i = 0; i < x; i++) {
+          const string = readString(buffer);
+          console.log(string);
+        }
+
+        let v25 = 0xb;
+        while (v25 === 0xb) {
+          v25 = buffer.readUInt8();
+          buffer.readUInt32LE(); // not used
+
+          if (v25 !== 0xb) {
+            break;
+          }
+
+          const v26 = buffer.readUInt16LE();
+          const string = readString(buffer);
+          console.log(string);
+        }
+        break;
+      }
+      // this is called either a MovieClip or an 'Animation Table'
       case 0x12:
+        // This is auto incremented
         const spriteId = buffer.readUInt16LE();
-        const numberOfRegions = buffer.readUInt16LE();
-        const totalNumberOfPoints = buffer.readUInt16LE();
+        // console.log(`spriteID: ${spriteId}`);
+        const numberOfSprites = buffer.readUInt16LE();
+        const totalNumberOfVertices = buffer.readUInt16LE();
 
         let innerBlockSize;
         while (innerBlockSize !== 0) {
-          const blockHeader = buffer.readUInt8(); // always 0x16=22??
+          const blockHeader = buffer.readUInt8(); // either 0x16=22 or 0x00=0
           innerBlockSize = buffer.readUInt32LE();
 
           if (innerBlockSize === 0) {
             break;
           }
 
+          console.log(`Type 12 block header: ${blockHeader}`);
+
           const spritesheetId = buffer.readUInt8();
-          const numberOfPoints = buffer.readUInt8();
+          const numberOfVertices = buffer.readUInt8();
           const coordinates = [];
 
-          for (let j = 0; j < numberOfPoints; j++) {
+          for (let j = 0; j < numberOfVertices; j++) {
             const x = buffer.readInt32LE();
             const y = buffer.readInt32LE();
-            coordinates.push([x / 10, y / 10]); // not sure
-            // if (i === 5) {
-            // console.log(`Coordinates: (${x}, ${y})`);
-            // }
+            coordinates.push([x * 0.05, y * 0.5]); // not sure
+            if (i === 5) {
+              console.log(`Coordinates: (${x}, ${y})`);
+            }
           }
 
           const polygon = [];
           // Polygon
-          for (let j = 0; j < numberOfPoints; j++) {
-            const x = Math.round(
+          for (let j = 0; j < numberOfVertices; j++) {
+            const x = Math.floor(
               (buffer.readUInt16LE() / 0xffff)
                 * spritesheets[spritesheetId].width,
             );
 
-            const y = Math.round(
+            const y = Math.floor(
               (buffer.readUInt16LE() / 0xffff)
                 * spritesheets[spritesheetId].height,
             );
@@ -109,9 +182,11 @@ const readNormalScFile = (buffer, spritesheets) => {
             // console.log(`Polygon: (${x}, ${y})`);
             // }
           }
-          // let image = new Jimp(
+
+          // if (i === 1) {
+          // const image = new Jimp(
           //   spritesheets[spritesheetId].width,
-          //   spritesheets[spritesheetId].height
+          //   spritesheets[spritesheetId].height,
           // );
           // let k = 0;
           // for (let i = 0; i < spritesheets[spritesheetId].height; i++) {
@@ -119,24 +194,28 @@ const readNormalScFile = (buffer, spritesheets) => {
           //     image.setPixelColor(
           //       spritesheets[spritesheetId].pixels[k++],
           //       j,
-          //       i
+          //       i,
           //     );
           //   }
           // }
-          // for (let j = 0; j < numberOfPoints; j++) {
-          //   for (let k = 0; k < 200; k++) {
+          // const radius = 32;
+          // for (let j = 0; j < numberOfVertices; j++) {
+          //   for (let k = 0; k < radius; k++) {
           //     image.setPixelColor(0xff0000ff, polygon[j][0] + k, polygon[j][1]);
-          //     image.setPixelColor(
-          //       0x00ff00ff,
-          //       polygon[j][0] + coordinates[j][0] + k,
-          //       polygon[j][1] + coordinates[j][1]
-          //     );
+          //     image.setPixelColor(0xff0000ff, polygon[j][0] - k, polygon[j][1]);
+          //     image.setPixelColor(0xff0000ff, polygon[j][0], polygon[j][1] + k);
+          //     image.setPixelColor(0xff0000ff, polygon[j][0], polygon[j][1] - k);
+          //     // image.setPixelColor(
+          //     //   0x00ff00ff,
+          //     //   polygon[j][0] + coordinates[j][0] + k,
+          //     //   polygon[j][1] + coordinates[j][1]
+          //     // );
           //   }
           // }
           // image.write(`out/test${spriteId}.png`, (err) => {
           //   if (err) throw err;
           // });
-          // break;
+          // }
         }
         break;
       default: {
@@ -266,13 +345,13 @@ const readScFile = async (scFileName) => {
 
 const main = async () => {
   fs.rmdirSync('out', { recursive: true });
-  const scFiles = fs.readdirSync('sc');
-  scFiles.forEach((scFile) => {
-    if (!scFile.endsWith('_tex.sc')) {
-      readScFile(scFile.substring(0, scFile.indexOf('.sc')));
-    }
-  });
-  // readScFile('events');
+  // const scFiles = fs.readdirSync('sc');
+  // scFiles.forEach((scFile) => {
+  //   if (!scFile.endsWith('_tex.sc')) {
+  //     readScFile(scFile.substring(0, scFile.indexOf('.sc')));
+  //   }
+  // });
+  readScFile('events');
 };
 
 main();
