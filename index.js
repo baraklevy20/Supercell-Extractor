3c129007be6e4577d032b9dbe3c53f235efba2e9
Barak Levy <baraklevy20@gmail.com>
2020-12-19 09:45:46 +0200

    Add yargs

--- index.js
+++ index.js
@@ -1,52 +1,44 @@
+/* eslint-disable no-nested-ternary */
 const fs = require('fs');
 require('./src/sc/scBuffer');
+const yargs = require('yargs/yargs');
+const { hideBin } = require('yargs/helpers');
 const { readScFile } = require('./src/sc/scFormat');
 const logger = require('./logger');
 
+const { argv } = yargs(hideBin(process.argv));
+
 const main = async () => {
-  fs.rmdirSync('out', { recursive: true });
-  fs.mkdirSync('out/Hay Day/1_49_4', { recursive: true });
-  fs.mkdirSync('out/sc', { recursive: true });
-  fs.mkdirSync('out/sccoc', { recursive: true });
   const startTime = new Date().getTime();
   const promises = [];
-  // const scFiles = fs.readdirSync('Hay Day/1_49_4');
-  // scFiles.forEach((scFile) => {
-  //   if (!scFile.endsWith('_tex.sc')) {
-  //     promises.push(readScFile(`Hay Day/1_49_4/${scFile.substring(0, scFile.indexOf('.sc'))}`));
-  //   }
-  // });
+  const filesToExtract = !argv.file ? [] : (Array.isArray(argv.file) ? argv.file : [argv.file]);
+  const folders = !argv.folder ? [] : (Array.isArray(argv.folder) ? argv.folder : [argv.folder]);
+  folders.forEach((folder) => {
+    // fs.rmdirSync(folder, { recursive: true });
+    // fs.mkdirSync(folder, { recursive: true });
+    const scFiles = fs.readdirSync(folder);
+    scFiles.forEach((scFile) => {
+      if (!scFile.endsWith('_tex.sc')) {
+        filesToExtract.push(`${folder}/${scFile}`);
+      }
+    });
+  });
 
-  // const scFiles = fs.readdirSync('sc');
-  // scFiles.forEach((scFile) => {
-  //   if (!scFile.endsWith('_tex.sc')) {
-  //     promises.push(readScFile(`sc/${scFile.substring(0, scFile.indexOf('.sc'))}`));
-  //   }
-  // });
+  fs.rmdirSync('out', { recursive: true });
 
-  // const scFiles = fs.readdirSync('sccoc');
-  // scFiles.forEach((scFile) => {
-  //   if (!scFile.endsWith('_tex.sc')) {
-  //     promises.push(readScFile(`sccoc/${scFile.substring(0, scFile.indexOf('.sc'))}`));
-  //   }
-  // });
-
-  // promises.push(readScFile('sc/effects_brawler'));
-  // promises.push(readScFile('Hay Day/1_49_4/common'));
-  // promises.push(readScFile('Hay Day/1_49_4/supercell_id'));
-  promises.push(readScFile('sc/events'));
-  // promises.push(readScFile('characters'));
-  // promises.push(readScFile('sc/supercell_id'));
-  // promises.push(readScFile('sc/loading'));
-  // promises.push(readScFile('sc/level'));
-  // promises.push(readScFile('sc/ui'));
-  // promises.push(readScFile('sc/debug'));
-  // promises.push(readScFile('background_basic'));
-  // promises.push(readScFile('sccoc/background_npc'));
+  filesToExtract.forEach((scFile) => {
+    fs.mkdirSync(`out/${scFile.substring(0, scFile.lastIndexOf('/'))}`, { recursive: true });
+    fs.mkdirSync(`sc_out/${scFile.substring(0, scFile.lastIndexOf('/'))}`, { recursive: true });
+    promises.push(readScFile(`${scFile.substring(0, scFile.indexOf('.sc'))}`));
+  });
 
   // const sharp = require('sharp');
-  // const ex = await sharp('./out/background_basic-texture2.png').metadata();
+  // const ex = await sharp('./out/sc/background_basic-shape0.png')
+  //   .affine([1, 0, 0, 1], { background: 'black', odx: 20 })
+  //   .affine([1, 0, 0, 1], { background: 'black', odx: -20 })
+  //   .toFile('result.png');
   const results = await Promise.allSettled(promises);
+  // const results = await Promise.all(promises);
   results.forEach((result) => {
     if (result.status === 'rejected') {
       // logger.error(result.reason);
