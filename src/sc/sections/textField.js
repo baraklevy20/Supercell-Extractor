2558fdf190f4db60625219c4c6ece698d563a196
barak.levy <baraklevy20@gmail.com>
2021-11-19 09:57:07 +0200

    Add more textfield fields

--- src/sc/sections/textField.js
+++ src/sc/sections/textField.js
@@ -3,33 +3,35 @@ const logger = require('../../../logger');
 const readTextField = (buffer, blockType) => {
   const exportId = buffer.readInt16LE();
   // logger.info(`TextField exportID: ${exportId}`);
-  const text = buffer.scReadString();
-  const textColor = buffer.readInt32LE(); // in ARGB
-  const flag1 = buffer.readUInt8(); // maybe text modifier - 0 or 1
-  const flag2 = buffer.readUInt8(); // maybe text modifier - 0 or 1
-  const flag3 = buffer.readUInt8(); // maybe text modifier - 0 or 1
-  const c4 = buffer.readUInt8(); // not sure if used
-  const c5 = buffer.readUInt8(); // if 2 => multiply by 2. otherwise, reduce by 3
-  const c6 = buffer.readUInt8(); // something is multiplied by this
-  const transform0 = buffer.readInt16LE();
-  const transform1 = buffer.readInt16LE();
-  const transform2 = buffer.readInt16LE();
-  const transform3 = buffer.readInt16LE();
-  const flag4 = buffer.readUInt8(); // maybe text modifier - 0 or 1
-  const text2 = buffer.scReadString();
+  const fontName = buffer.scReadString();
+  const fontColorInARGB = buffer.readInt32LE();
+  const isBold = !!buffer.readUInt8();
+  const isItalic = !!buffer.readUInt8();
+  const isMultiLine = !!buffer.readUInt8();
+  buffer.readUInt8(); // not used
+  // if 2 => multiply by 2. otherwise, reduce by 3.
+  // Seems like a binary flag. Maybe margin or something
+  const unknownC5 = buffer.readUInt8();
+  const fontSize = buffer.readUInt8();
+  const left = buffer.readInt16LE();
+  const top = buffer.readInt16LE();
+  const right = buffer.readInt16LE();
+  const bottom = buffer.readInt16LE();
+  const isUppercase = buffer.readUInt8();
+  const unknownText2 = buffer.scReadString();
 
-  let flag5;
-  let anotherColor;
+  let unknownFlag5;
+  let outlineColor;
   let c14;
   let c15;
 
   if (blockType !== 0x07) {
-    flag5 = buffer.readUInt8(); // maybe text modifier - 0 or 1
+    unknownFlag5 = !!buffer.readUInt8(); // maybe text modifier - 0 or 1
 
     if (blockType === 0x15) {
-      anotherColor = buffer.readUInt32LE();
+      outlineColor = buffer.readUInt32LE();
     } else if (blockType === 0x21) {
-      anotherColor = buffer.readUInt32LE();
+      outlineColor = buffer.readUInt32LE();
       c14 = buffer.readInt16LE();
       c15 = buffer.readInt16LE();
     } else if (blockType === 0x2c) {
@@ -40,27 +42,25 @@ const readTextField = (buffer, blockType) => {
   const textField = {
     exportId,
     type: 'textField',
-    text,
-    text2,
-    textColor,
-    flag1,
-    flag2,
-    flag3,
-    c4,
-    c5,
-    c6,
-    transform0,
-    transform1,
-    transform2,
-    transform3,
-    flag4,
-    flag5,
-    anotherColor,
+    fontName,
+    text2: unknownText2,
+    fontColorInARGB,
+    isBold,
+    isItalic,
+    isMultiLine,
+    c5: unknownC5,
+    fontSize,
+    transform0: left,
+    transform1: top,
+    transform2: right,
+    transform3: bottom,
+    isUppercase,
+    flag5: unknownFlag5,
+    outlineColor,
     c14,
     c15,
   };
 
-  // logger.debug(textField);
   return textField;
 
   // logger.info('TextField: ', textField);
