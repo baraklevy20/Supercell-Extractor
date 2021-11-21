10569220cff6bbbf6e3614dad88cac2abbb1f716
barak.levy <baraklevy20@gmail.com>
2021-11-21 15:23:49 +0200

    Fix color transform order

--- src/sc/sections/colorVariation.js
+++ src/sc/sections/colorVariation.js
@@ -1,12 +1,12 @@
 const readColorTransform = (buffer) => {
   const colorTransform = {
-    blueAddition: buffer.readUInt8(),
-    greenAddition: buffer.readUInt8(),
     redAddition: buffer.readUInt8(),
+    greenAddition: buffer.readUInt8(),
+    blueAddition: buffer.readUInt8(),
     alphaMultiplier: buffer.readUInt8(),
-    blueMultiplier: buffer.readUInt8(),
-    greenMultiplier: buffer.readUInt8(),
     redMultiplier: buffer.readUInt8(),
+    greenMultiplier: buffer.readUInt8(),
+    blueMultiplier: buffer.readUInt8(),
   };
   return colorTransform;
 };
